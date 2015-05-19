9a837871840f16dbe768fb84ac86002cdbaec630
Marcel Ebmer <marcel@26elf.at>
2015-05-19 23:10:51 +0200

    make_true -> make_false

--- boost/with.hpp
+++ boost/with.hpp
@@ -3,59 +3,50 @@
 
 #include <utility>
 #include <type_traits>
-#include <boost/config.hpp>
-
-#define BOOST_WITH_UNUSED_ARG                                                  \
-    boost_with_INTENTIONALLY_UNUSED BOOST_ATTRIBUTE_UNUSED
 
 #define BOOST_WITH(what)                                                       \
-    if (auto BOOST_WITH_UNUSED_ARG = boost::with_detail::make_true(what))
+    if (auto boost_with_intentionally_unused                                   \
+        = boost::with_detail::make_false(what)) {                              \
+        (void) boost_with_intentionally_unused;                                \
+    } else
 
 
 namespace boost {
 namespace with_detail {
 
 // wraps an object of movable type
-// and makes it convertible to bool (always true)
+// and makes it convertible to bool (always false)
 template <class T>
-class always_true {
+class always_false {
 public:
-    explicit always_true(T what) : x{std::move(what)} {}
-    constexpr operator bool() const { return true; }
+    explicit always_false(T what) : x{std::move(what)} {}
+    constexpr operator bool() const { return false; }
 
 private:
     T x;
 };
 
-// always_true<T> construction helper
+// always_false<T> factory
 template <class T>
-auto make_true(T&& what, std::true_type) {
+auto make_false(T&& what, std::true_type) {
     static_assert(std::is_move_constructible<T>::value,
                   "this is a BOOST_WITH bug");
-    return always_true<T>{std::forward<T>(what)};
+    return always_false<T>{std::forward<T>(what)};
 }
 
 // static error for non-movable types
 template <class T>
-auto make_true(T&&, std::false_type) {
+auto make_false(T&&, std::false_type) {
     static_assert(std::is_move_constructible<T>::value,
                   "BOOST_WITH requires the scoped object's type to be move "
                   "constructible");
-    return std::true_type{}; // never reached
+    return std::false_type{}; // never reached
 }
 
 // macro entry point, tag dispatch for static type checking
-// NOTE: the static_assert has to be hidden away from the #define, because
-// otherwise the construct
-//
-// for(;;)
-//     BOOST_WITH(0) {}
-//
-// would not work (note missing curly brace after 'for')
-//
 template <class T>
-auto make_true(T&& what) {
-    return make_true(std::forward<T>(what), std::is_move_constructible<T>{});
+auto make_false(T&& what) {
+    return make_false(std::forward<T>(what), std::is_move_constructible<T>{});
 }
 }
 } // namepace boost::with_detail
