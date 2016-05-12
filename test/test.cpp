bdea393117a40a24d6bf5346349b797ca94f8770
Marcel Ebmer <marcel@26elf.at>
2016-05-12 08:11:59 +0200

    tests updated

--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,7 @@
 #include "boost/with.hpp"
 #include <mutex>
 
-class Lockable {
+class Dummy_lock {
 public:
     void lock() { l = true; }
     void unlock() { l = false; }
@@ -12,44 +12,62 @@ private:
     bool l = false;
 };
 
-using Unique_lock = std::unique_lock<Lockable>;
+using Lock_guard = std::lock_guard<Dummy_lock>;
 
-TEST(BoostWithTest, TestIfStmt) {
-    BOOST_WITH(int, 0) {}
-    BOOST_WITH(int) {}
+class Copyable {
+public:
+    Copyable(int, int) {}
+    Copyable(Copyable const&) = default;
+    Copyable(Copyable&&) = delete;
+};
+
+class Movable {
+public:
+    Movable() {}
+    Movable(int, int, int) {}
+    Movable(Movable const&) = delete;
+    Movable(Movable&&) = default;
+};
+
+TEST(BoostWith, Basic) {
+    BOOST_WITH (int, 0) {}
+    BOOST_WITH (int) {}
+    BOOST_WITH (Copyable, 1, 2) {}
+    BOOST_WITH (Movable, 1, 2, 3) {}
+    BOOST_WITH (Movable) {}
 }
 
-TEST(BoostWithTest, TestGuard) {
-    Lockable l;
+TEST(BoostWith, Guard) {
+    Dummy_lock l;
     EXPECT_FALSE(l.locked());
-    BOOST_WITH(Unique_lock, l)
+    BOOST_WITH (Lock_guard, l)
         EXPECT_TRUE(l.locked());
     EXPECT_FALSE(l.locked());
 }
 
-TEST(BoostWithTest, TestNesting) {
-    Lockable l1;
-    Lockable l2;
-    BOOST_WITH(Unique_lock, l1)
-        BOOST_WITH(Unique_lock, l2) {
+TEST(BoostWith, Nesting) {
+    Dummy_lock l1;
+    Dummy_lock l2;
+    BOOST_WITH (Lock_guard, l1)
+        BOOST_WITH (Lock_guard, l2) {
             EXPECT_TRUE(l1.locked());
             EXPECT_TRUE(l2.locked());
         }
     EXPECT_FALSE(l1.locked());
     EXPECT_FALSE(l2.locked());
 
-    BOOST_WITH(Unique_lock, l1) {
-        BOOST_WITH(Unique_lock, l2)
+    BOOST_WITH (Lock_guard, l1) {
+        BOOST_WITH (Lock_guard, l2)
             EXPECT_TRUE(l2.locked());
         EXPECT_TRUE(l1.locked());
         EXPECT_FALSE(l2.locked());
     }
 
-    for (auto i = 0; i < 3; ++i)
-        BOOST_WITH(Unique_lock, l1)
+    for (auto i = 0; i < 2; ++i)
+        BOOST_WITH (Lock_guard, l1)
             EXPECT_TRUE(l1.locked());
 
-    BOOST_WITH(Unique_lock, l1)
-        for (auto i = 0; i < 10; ++i)
+    BOOST_WITH (Lock_guard, l1)
+        for (auto i = 0; i < 2; ++i)
             EXPECT_TRUE(l1.locked());
 }
