71dcc1467ca386dc1d0ba3d2feb4d31a0721fb11
Marcel Ebmer <marcel@26elf.at>
2016-05-12 06:27:15 +0200

    enable non-movable types

--- boost/with.hpp
+++ boost/with.hpp
@@ -2,11 +2,12 @@
 #define BOOST_WITH_HPP_INCLUDED
 
 #include <utility>
+#include <cassert>
 #include <type_traits>
 
-#define BOOST_WITH(what)                                                       \
-    if (auto boost_with_intentionally_unused                                   \
-        = boost::with_detail::make_false(what)) {                              \
+#define BOOST_WITH(T, ...)                                                     \
+    if (boost::with_detail::always_false<T> boost_with_intentionally_unused    \
+        = {__VA_ARGS__}) {                                                     \
         (void) boost_with_intentionally_unused;                                \
     } else
 
@@ -14,40 +15,19 @@
 namespace boost {
 namespace with_detail {
 
-// wraps an object of movable type
-// and makes it convertible to bool (always false)
+// makes anything convertible to bool (always false)
 template <class T>
 class always_false {
 public:
-    explicit always_false(T what) : x{std::move(what)} {}
+    template <class... Args>
+    always_false(Args&&... args)
+        : x(std::forward<Args>(args)...) {}
+
     constexpr operator bool() const { return false; }
 
 private:
     T x;
 };
-
-// always_false<T> factory
-template <class T>
-auto make_false(T&& what, std::true_type) {
-    static_assert(std::is_move_constructible<T>::value,
-                  "this is a BOOST_WITH bug");
-    return always_false<T>{std::forward<T>(what)};
-}
-
-// static error for non-movable types
-template <class T>
-auto make_false(T&&, std::false_type) {
-    static_assert(std::is_move_constructible<T>::value,
-                  "BOOST_WITH requires the scoped object's type to be move "
-                  "constructible");
-    return std::false_type{}; // never reached
-}
-
-// macro entry point, tag dispatch for static type checking
-template <class T>
-auto make_false(T&& what) {
-    return make_false(std::forward<T>(what), std::is_move_constructible<T>{});
-}
 }
 } // namepace boost::with_detail
 
--- test/test.cpp
+++ test/test.cpp
@@ -15,13 +15,14 @@ private:
 using Unique_lock = std::unique_lock<Lockable>;
 
 TEST(BoostWithTest, TestIfStmt) {
-    BOOST_WITH(0) {}
+    BOOST_WITH(int, 0) {}
+    BOOST_WITH(int) {}
 }
 
 TEST(BoostWithTest, TestGuard) {
     Lockable l;
     EXPECT_FALSE(l.locked());
-    BOOST_WITH(Unique_lock(l))
+    BOOST_WITH(Unique_lock, l)
         EXPECT_TRUE(l.locked());
     EXPECT_FALSE(l.locked());
 }
@@ -29,26 +30,26 @@ TEST(BoostWithTest, TestGuard) {
 TEST(BoostWithTest, TestNesting) {
     Lockable l1;
     Lockable l2;
-    BOOST_WITH(Unique_lock(l1))
-        BOOST_WITH(Unique_lock(l2)) {
+    BOOST_WITH(Unique_lock, l1)
+        BOOST_WITH(Unique_lock, l2) {
             EXPECT_TRUE(l1.locked());
             EXPECT_TRUE(l2.locked());
         }
     EXPECT_FALSE(l1.locked());
     EXPECT_FALSE(l2.locked());
 
-    BOOST_WITH(Unique_lock(l1)) {
-        BOOST_WITH(Unique_lock(l2))
+    BOOST_WITH(Unique_lock, l1) {
+        BOOST_WITH(Unique_lock, l2)
             EXPECT_TRUE(l2.locked());
         EXPECT_TRUE(l1.locked());
         EXPECT_FALSE(l2.locked());
     }
 
     for (auto i = 0; i < 3; ++i)
-        BOOST_WITH(Unique_lock(l1))
+        BOOST_WITH(Unique_lock, l1)
             EXPECT_TRUE(l1.locked());
 
-    BOOST_WITH(Unique_lock(l1))
+    BOOST_WITH(Unique_lock, l1)
         for (auto i = 0; i < 10; ++i)
             EXPECT_TRUE(l1.locked());
 }
